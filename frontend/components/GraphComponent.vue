82f44ea625bfc91816d51f30b54c62153c6f3f4e
Julian Eckhardt <julian@eckhardt.io>
2021-05-21 18:49:38 +0200

    WIP cleanup

--- frontend/components/GraphComponent.vue
+++ frontend/components/GraphComponent.vue
@@ -12,98 +12,99 @@
 </template>
 
 <script>
-const jsonSchema = [
-  {
-    name: 'index',
-    type: 'date',
-    format: '%Y-%d-%m',
-  },
-  {
-    name: 'Gesamt',
-    type: 'number',
-  },
-  {
-    name: 'Reduzierte_Gesamt',
-    type: 'number',
-  },
-]
-
-const jsonData = [
-  {
-    index: '2020-05-06',
-    Gesamt: 5980,
-    Reduzierte_Gesamt: 5120,
-  },
-  {
-    index: '2020-05-07',
-    Gesamt: 5705,
-    Reduzierte_Gesamt: 5090,
-  },
-  {
-    index: '2020-05-08',
-    Gesamt: 5394,
-    Reduzierte_Gesamt: 5001,
-  },
-  {
-    index: '2020-05-09',
-    Gesamt: 5703,
-    Reduzierte_Gesamt: 4120,
-  },
-  {
-    index: '2020-05-10',
-    Gesamt: 5715,
-    Reduzierte_Gesamt: 3120,
-  },
-]
-
-const dataSource = {
-  chart: {
-    caption: 'Covid Fallzahlen',
-    subcaption: 'Gesamtfallzahlen',
-    series: 'index',
-    xaxisname: 'Datum',
-    yaxisname: 'Fallzahlen in 1000',
-    numbersuffix: 'K',
-    theme: 'fusion',
-    yAxis: [
-      {
-        plot: [
-          {
-            value: 'Gesamt',
-            type: 'line',
-          },
-          {
-            value: 'Reduzierte_Gesamt',
-            type: 'column',
-          },
-        ],
-      },
-    ],
-  },
-  data: null,
-}
-
 export default {
   data() {
     return {
       type: 'timeseries',
       renderAt: 'chart-container',
-      width: '550',
-      height: '550',
+      width: '100%',
+      height: '100%',
       dataFormat: 'json',
-      dataSource,
+      dataSource: {
+        chart: {
+          caption: 'Covid Fallzahlen',
+          subcaption: 'Gesamtfallzahlen',
+          series: 'index',
+          xaxisname: 'Datum',
+          yaxisname: 'Fallzahlen in 1000',
+          numbersuffix: 'K',
+          theme: 'fusion',
+          yAxis: [
+            {
+              plot: [
+                {
+                  value: 'Gesamt',
+                  type: 'line',
+                },
+                {
+                  value: 'Reduzierte_Gesamt',
+                  type: 'column',
+                },
+              ],
+            },
+          ],
+        },
+        data: null,
+      },
+      schema: [
+        {
+          name: 'index',
+          type: 'date',
+          format: '%Y-%d-%m',
+        },
+        {
+          name: 'Gesamt',
+          type: 'number',
+        },
+        {
+          name: 'Reduzierte_Gesamt',
+          type: 'number',
+        },
+      ],
+      jsonData: [
+        {
+          index: '2020-05-06',
+          Gesamt: 5980,
+          Reduzierte_Gesamt: 5120,
+          foo: 'bar',
+        },
+        {
+          index: '2020-05-07',
+          Gesamt: 5705,
+          Reduzierte_Gesamt: 5090,
+        },
+        {
+          index: '2020-05-08',
+          Gesamt: 5394,
+          Reduzierte_Gesamt: 5001,
+        },
+        {
+          index: '2020-05-09',
+          Gesamt: 5703,
+          Reduzierte_Gesamt: 4120,
+        },
+        {
+          index: '2020-05-10',
+          Gesamt: 5715,
+          Reduzierte_Gesamt: 3120,
+        },
+      ],
     }
   },
   mounted() {
     const FusionCharts = require('fusioncharts')
 
-    const data = jsonData
-    const schema = jsonSchema
     const fusionTable = new FusionCharts.DataStore().createDataTable(
-      data,
-      schema
+      this.jsonData,
+      this.schema
     )
     this.dataSource.data = fusionTable
   },
 }
 </script>
+
+<style scoped>
+#chart-container {
+  height: 80vh;
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,55 +1,62 @@
 <template>
-  <div class="container">
+  <b-container fluid class="mt-5">
     <b-row>
-      <b-col class="graphCol">
-        <GraphComponent />
+      <b-col class="graphCol" cols="10">
+        <client-only>
+          <GraphComponent />
+        </client-only>
       </b-col>
-      <b-col class="formCol">
-        <b-form class="forms">
-          <b-form-group
-            id="rReductionValue"
-            label="R-Zahl-Reduktion:"
-            label-for="rReductionInput"
-          >
-            <b-form-input
-              id="rInput"
-              v-model="rReductionValue"
-              class="rReductionInput"
-              placeholder="Z.B. 1.2"
-              required
-              type="text"
-            />
-          </b-form-group>
+      <b-col class="formCol" cols="2">
+        <b-card class="bg-light">
+          <h3>Parametereingabe</h3>
+          <b-form class="forms mt-4">
+            <b-form-group
+              id="rReductionValue"
+              label="R-Zahl-Reduktion:"
+              label-for="rReductionInput"
+            >
+              <b-form-input
+                id="rInput"
+                v-model="rReductionValue"
+                class="rReductionInput"
+                placeholder="Z.B. 1.2"
+                required
+                type="text"
+              />
+            </b-form-group>
 
-          <b-form-group
-            id="timeFrameStart"
-            label="Startzeitpunkt:"
-            label-for="timeFrameStartInput"
-          >
-            <b-form-input
-              id="timeFrameStartInput"
-              v-model="timeFrameStart"
-              placeholder="Datum eingeben"
-              type="date"
-            />
-          </b-form-group>
-          <b-form-group
-            id="timeFrameEnd"
-            label="Endzeitpunkt:"
-            label-for="timeFrameEndInput"
-          >
-            <b-form-input
-              id="timeFrameEndInput"
-              v-model="timeFrameEnd"
-              placeholder="Datum eingeben"
-              type="date"
-            />
-          </b-form-group>
-          <b-button variant="primary"> Senden </b-button>
-        </b-form>
+            <b-form-group
+              id="timeFrameStart"
+              label="Startzeitpunkt:"
+              label-for="timeFrameStartInput"
+            >
+              <b-form-input
+                id="timeFrameStartInput"
+                v-model="timeFrameStart"
+                placeholder="Datum eingeben"
+                type="date"
+              />
+            </b-form-group>
+            <b-form-group
+              id="timeFrameEnd"
+              label="Endzeitpunkt:"
+              label-for="timeFrameEndInput"
+            >
+              <b-form-input
+                id="timeFrameEndInput"
+                v-model="timeFrameEnd"
+                placeholder="Datum eingeben"
+                type="date"
+              />
+            </b-form-group>
+            <b-button variant="primary" class="float-right"
+              >Aktualisieren</b-button
+            >
+          </b-form>
+        </b-card>
       </b-col>
     </b-row>
-  </div>
+  </b-container>
 </template>
 
 <script>
@@ -61,24 +68,15 @@ export default {
   },
   data() {
     return {
-      rReductionValue: null,
       timeFrameStart: null,
       timeFrameEnd: null,
+      rReductionValue: null,
     }
   },
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
