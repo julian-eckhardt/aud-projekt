beaa283714a0993755e2d6470570ac5a6e837e38
Ervin Joa <ejoa@techfak.uni-bielefeld.de>
2021-07-18 10:42:11 +0200

    Added Doc for all Methods in Backend

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,34 @@
 import json
 
-from flask import Flask, jsonify, request
+from flask import Flask, request
 from flask_cors import CORS
 
 from data_loader import data_from_params, load_bundesländer, load_data
 
+# Initialisierung des Flask-Servers
 app = Flask(__name__)
+
+# Einstellung von CORS auf die Adresse der Oberfläche
 cors = CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
+'''
+@author Ervin Joa, Julian Eckhardt
+
+API-Call um die Original- und Simulierte-Daten zu holen.
+
+Nimmt keine oder 3 Parameter entgegen, welche die Simulation
+berechnen.
+
+Ein weiterer Parameter wird verwendet um die Simulation für ein bestimmtes
+Bundesland zu beobachten.
+
+@param start_date: Start-Zeitpunkt der Reduktion
+@param end_date: End-Zeitpunkt der Reduktion
+@param r_reduction_value: R-Reduktionswert
+@param bundesland: Filtrierung nach einem bestimmten Bundesland
+
+@return: JSON-Objekt als Daten, Ergebniss ist von den Parametern abhängig.
+'''
 @app.route('/data', methods=['GET'])
 def get_data_with_params():
     start_date = request.args.get("timeFrameStart")
@@ -15,13 +36,21 @@ def get_data_with_params():
     r_reduction_value = request.args.get("rReductionValue")
     bundesland = request.args.get("bundesland")
 
+    # Wenn alle Parameter eingegeben wurden, dann Simulation beginnen
     if start_date is not None and end_date is not None and r_reduction_value is not None: 
       result = data_from_params(start_date, end_date, r_reduction_value, bundesland)
       return result.to_json(orient="records", date_format="iso")
 
-    # otherwise just return plain data
+    # Ansonsten gebe Original-Daten zurück 
     return load_data(bundesland).to_json(orient="records", date_format="iso")
 
+'''
+@author Ervin Joa, Julian Eckhardt
+
+API-Call, welcher die Liste der Bundesländer zurück gibt.
+
+@return: JSON-Objekt mit der Liste der Bundeländer.
+'''
 @app.route('/bundeslaender', methods=['GET'])
 def get_bundesländer():
     index = load_bundesländer()
--- backend/data_loader.py
+++ backend/data_loader.py
@@ -1,38 +1,81 @@
 from datetime import datetime
 
-import numpy as np
 import pandas as pd
 
+# Laden der Daten vom RKI
 covid_data = pd.read_excel('data/Fallzahlen_Kum_Tab.xlsx', sheet_name='BL_7-Tage-Fallzahlen', skiprows=2, index_col=0)
 
+'''
+@author Ervin Joa, Julian Eckhardt
+
+Eine Methode, welche die Bundesländer als Liste zurückgibt.
+
+@return: Indexes eines DataFrames als Liste
+'''
 def load_bundesländer():
     df = covid_data.copy()
     return df.index.tolist()
 
+
+'''
+@author Ervin Joa, Julian Eckhardt
+
+Eine Methode zum Zurückgeben der Daten und Filtern der Daten nach Bundesland
+(falls dies ausgewählt wurde).
+
+@param bundesland: Name des jeweiligen Bundeslandes.
+
+@return: Ein DataFrame mit den Fallzahlen, mit oder ohne des gewünschten Bundeslandes
+'''
 def load_data(bundesland):
     df = covid_data.copy()
     df = df.T
     df.index = pd.to_datetime(df.index)
     df.reset_index(inplace=True)
 
+    ## Falls ein Bundesland ausgewählt wurde, werden die Fallzahlen 
+    # von disem betrachtet
     if bundesland is not None:
         df["Gesamt"] = df[bundesland]
 
+    # Schmeiße die restlichen Bundesländer raus
     df.drop(columns=["Baden-Württemberg", "Bayern", "Berlin",
                             "Brandenburg", "Bremen", "Hamburg", "Hessen", "Mecklenburg-Vorpommern", "Niedersachsen",
                             "Nordrhein-Westfalen", "Rheinland-Pfalz", "Saarland", "Sachsen", "Sachsen-Anhalt", "Schleswig-Holstein",
                             "Thüringen"], inplace=True)
+    
+    # Umbenennung der Spalte, für die Darstellung des Graphen
     df = df.rename(columns={"Gesamt": "Fallzahlen"})
+
+    # Den Typ der Daten als Original markieren
     df["Type"] = "Original Fallzahlen"
     return df
 
+'''
+@author Ervin Joa, Julian Eckhardt
 
+Folgende Methode führt die Simulation, der Fallzahl-Reduktion, durch.
+Sollte ein Bundesland ausgeählt worden sein, dann wird die Reduktion
+auf diesem durchgeführt.
+
+@param start_date: Start-Zeitpunkt der Reduktion
+@param end_date: End-Zeitpunkt der Reduktion
+@param r_reduction_value: R-Reduktionswert
+@param bundesland: Filtrierung nach einem bestimmten Bundesland
+
+@return: Ergebniss der Reduktion als DataFrame
+
+'''
 def data_from_params(start_date, end_date, r_reduction_value, bundesland):
 
+    # Festlegung des Datumformates
     date_format = "%Y-%m-%d"
+
+    # Erstellung der Start- und End-Zeiträume
     start_date_timestamp = datetime.strptime(start_date, date_format)
     end_date_timestamp = datetime.strptime(end_date, date_format)
 
+    # Falls BUndesland ausgewählt, werden die Daten von diesem verwendet
     df = load_data(bundesland)
 
     # Kopieren von Daten
@@ -43,15 +86,12 @@ def data_from_params(start_date, end_date, r_reduction_value, bundesland):
 
     # Berechnen der Änderungsraten für gesamten Zeitraum
     for index, row in df_result.iterrows():
-        # print("index " + str(index) + " row " + str(row))
         if index + 1 < len(df_result):
             # Werte berechnen
             cases_today = df_result.at[index,"Fallzahlen Original"]
             cases_tomorrow = df_result.at[index + 1,"Fallzahlen Original"]
             rate_of_change = cases_tomorrow / cases_today
-            # print(f"size: {len(df_result)} index: {index} today: {cases_today} tomorrow: {cases_tomorrow} rate of change: {rate_of_change}")
             df_result.at[index,"Änderungsrate"] = rate_of_change
-            # print(f"{df_result.iloc[index]}")
 
             # Neue Fallzahlen berechnen und setzen
             if index == 0:
@@ -63,7 +103,6 @@ def data_from_params(start_date, end_date, r_reduction_value, bundesland):
                     rate_of_change_new = df_result.at[index -1, "Änderungsrate"] * (1 - float(r_reduction_value))
                 else:
                     rate_of_change_new = df_result.at[index -1, "Änderungsrate"]
-                #print(f"Änderung alt: {df_result.at[index - 1, 'Änderungsrate']} neu: {rate_of_change_new}")
                 print(df_result.iloc[index])
                 df_result.at[index, "Fallzahlen"] = round(
                     df_result.at[index - 1, "Fallzahlen"] * rate_of_change_new
